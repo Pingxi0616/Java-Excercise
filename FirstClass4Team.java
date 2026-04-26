93e79150eab4311c1a348ad9421fb8e095470c20
willy23242 <153217761+willy23242@users.noreply.github.com>
2026-04-26 10:41:25 +0800

    Add student information for willy

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team{
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357149\"\n姓名：\"林品希\"");
         System.out.println("\"學號：\"01157016\"\n姓名：\"鍾佳芯\"");
+        System.out.println("\"學號：\"01157067\"\n姓名：\"丘浩泓\"");
     }
 }
